338ec2dd7e4ba9c5cccf0c56c065118c86218dd8
JPENASO <84899665+jpenaso10@users.noreply.github.com>
2024-09-18 23:59:25 +0800

    main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Teknoys! 
+          Welcome Teknoys!
         </p>
         <a
           className="App-link"
