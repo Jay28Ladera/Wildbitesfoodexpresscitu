1fc42d85879a19152de7dca6e00deb1781a8afd2
emmskii <emmanuel.amomonpon@gmail.com>
2024-09-25 07:10:04 +0800

    [Added] Transitions and More
    - Added Transitions, Redirects, Signup Confirmation.
    - Separated Login and Sign up.
    - Updated some minor fonts and positions.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Homepage from './components/homepage'; 
-import Login from './components/login';       
-import UserProfile from './components/UserProfile';
-import ForgotPassword from './components/forgotpassword';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from 'framer-motion';
+import Homepage from "./components/homepage";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import UserProfile from "./components/UserProfile";
+import ForgotPassword from "./components/forgotpassword";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgotpassword" element={<ForgotPassword/>}/>
-                <Route path="/profile" element={<UserProfile />} /> 
+  const location = useLocation(); // useLocation hook needs to be inside a Router
 
-            </Routes>
-        </Router>
-    );
+  return (
+    <AnimatePresence mode="wait"> 
+      {/* AnimatePresence wraps your Routes to enable page transitions */}
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWrapper;
