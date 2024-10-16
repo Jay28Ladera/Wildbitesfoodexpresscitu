dee61f3324c9dc35a6dc7fb0d8ad7955ff832c1f
Jay28Ladera <149279350+Jay28Ladera@users.noreply.github.com>
2024-10-16 20:41:37 +0800

    adjustment/admincss

--- src/components/signup.js
+++ src/components/signup.js
@@ -125,8 +125,8 @@ function Signup() {
       className="signup-container"
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -100 }}
-      transition={{ duration: 0.5 }}
+      exit={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0 }}
     >
       <header className="signup-header">
         <Link to="/">
